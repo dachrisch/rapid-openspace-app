073f03984f16b66e5faf21446a811ac5059b8e3a
Christian Dähn <christian.daehn@gmail.com>
2021-01-29 17:45:33 +0100

    using waitress

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ import os
 from rapidos.web import create_app
 
 
+def run_waitress():
+    app = create_app()
+    return app
+
+
 def run_flask():
     app = create_app()
     [print(repr(p)) for p in app.url_map.iter_rules()]
