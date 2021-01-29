5db0f54fa16ed2eaa8158797b05216fa96bea74e
Christian Dähn <christian.daehn@gmail.com>
2021-01-29 17:42:27 +0100

    heroku setup

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from rapidos.web import create_app
 
 
@@ -5,7 +7,7 @@ def run_flask():
     app = create_app()
     [print(repr(p)) for p in app.url_map.iter_rules()]
 
-    app.run(debug=True)
+    app.run(debug=True, port=os.getenv('PORT', 5000))
 
 
 if __name__ == '__main__':
